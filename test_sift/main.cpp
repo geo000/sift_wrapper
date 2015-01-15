7adfb580bf1a939918d73ec325ae57800fcf1526
kassol <kassol.zx@gmail.com>
2015-01-15 14:11:18 +0800

    Empty the vector.

--- test_sift/main.cpp
+++ test_sift/main.cpp
@@ -6,14 +6,18 @@ int main(int argc, char* argv[])
 	Sift* sift = Sift::GetInstance();
 	std::vector<SiftGPU::SiftKeypoint> keys1;
 	std::vector<float> descriptor1;
-	sift->GetKeyPoints("C:\\data\\A1.tif", keys1, descriptor1);
+	sift->GetKeyPoints("C:\\data\\110270110034.tif", keys1, descriptor1);
 	
 	std::vector<SiftGPU::SiftKeypoint> keys2;
 	std::vector<float> descriptor2;
-	sift->GetKeyPoints("C:\\data\\A2.tif", keys2, descriptor2);
+	sift->GetKeyPoints("C:\\data\\110270111031.tif", keys2, descriptor2);
 
 	std::vector<SamePoint> result;
-	sift->GetMatchedPoints(keys1, &descriptor1[0], keys2, &descriptor2[0], result);
+	printf("Matched %d points.\n", sift->GetMatchedPoints(keys1, &descriptor1[0], keys2, &descriptor2[0], result));
+	keys1.swap(std::vector<SiftGPU::SiftKeypoint> ());
+	keys2.swap(std::vector<SiftGPU::SiftKeypoint> ());
+	descriptor1.swap(std::vector<float> ());
+	descriptor2.swap(std::vector<float> ());
 	Sift::Release();
 
 	return 0;
